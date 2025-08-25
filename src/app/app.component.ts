3e86e53440ceef646f2abdb52ca519d459fdab18
PrinceDevSoftrefine <ubuntubhai.developer@gmail.com>
2025-08-25 10:24:19 +0530

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,10 @@ export class AppComponent implements OnInit {
   isLoading: boolean = true;
 
   async ngOnInit(): Promise<void> {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    // console.log(this.userService.allUsers());
-
     await this.userService.fetchUsers();
     this.isLoading = false;
     console.log(this.userService.allUsers());
   }
 
-  async onFetchData() {
-    //   console.log(
-    //     await this.userService.fetchUsers('https://dummyjson.com/users')
-    //   );
-    //   // console.log(this.userService);
-  }
+  async onFetchData() {}
 }
