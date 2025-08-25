c5d8188230a7e1f50a04b1d89ebdb5c460fd98ee
PrinceDevSoftrefine <ubuntubhai.developer@gmail.com>
2025-08-25 16:13:05 +0530

    optimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { DataTableServiceService } from './core/services/data-table-service.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  userService = inject(DataTableServiceService);
-
-  async ngOnInit() {
-    await this.userService.fetchUsers();
-  }
-}
+export class AppComponent {}
